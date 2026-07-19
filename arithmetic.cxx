4650c9bb021f4efc76fa69b90c3374fa204cfea1
vijipravasthi-pixel <vijipravasthi@gmail.com>
2026-07-19 11:35:53 +0530

    . c

---
+++ arithmetic.cxx
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int a=10;
+    int b=3;
+    printf("a=%d,b=%d\n",a,b);
+    printf("Addition: %d\n",a+b);
+    printf("Subtraction: %d\n",a-b);
+    printf("Multiplication: %d\n",a*b);
+    printf("Division: %d\n",a/b);
+    printf("Remainder: %d\n",a%b);
+    return 0;
+}
