52d46b54c2591ba456d3cfe7a5e461e7f19e1a32
vijipravasthi-pixel <vijipravasthi@gmail.com>
2026-07-19 11:36:58 +0530

    . c

---
+++ variables.cxx
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    char name[]="Viji";
+    int age=25;
+    printf("Hello,%s!\n",name);
+    printf("Welcome to c programming\n");
+    printf("Your age is:%d\n",age);
+    return 0;
+}
