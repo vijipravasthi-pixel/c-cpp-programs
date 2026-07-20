bbb93b21e52fea971faea823a87e619ef30f7e96
vijipravasthi-pixel <vijipravasthi@gmail.com>
2026-07-20 19:53:54 +0530

    . c

---
+++ variable.cxx
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int a = 10;
+    float b = 5.5;
+    char c = 'A';
+    double d = 3.14159;
+
+    printf("Integer: %d\n", a);
+    printf("Float: %.2f\n", b);
+    printf("Character: %c\n", c);
+    printf("Double: %.5f\n", d);
+
+    return 0;
+}
